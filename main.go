f0481ece35c7a43be57246599a7a7aa52fff61c5
jfrbast <bastien.jouffre@ynov.com>
2025-08-13 18:52:44 +0200

    reset push

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	http.HandleFunc("/", pages.HomePage)
+	http.HandleFunc("/about", pages.AboutPage)
+	http.HandleFunc("/credits", pages.CreditsPage)
 
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- pages/pages.go
+++ pages/pages.go
@@ -13,3 +13,18 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Erreur serveur", http.StatusInternalServerError)
 	}
 }
+
+func AboutPage(w http.ResponseWriter, r *http.Request) {
+	err := templates.Temp.ExecuteTemplate(w, "about", nil)
+	if err != nil {
+		log.Println("Erreur template accueil:", err)
+		http.Error(w, "Erreur serveur", http.StatusInternalServerError)
+	}
+}
+func CreditsPage(w http.ResponseWriter, r *http.Request) {
+	err := templates.Temp.ExecuteTemplate(w, "credits", nil)
+	if err != nil {
+		log.Println("Erreur template accueil:", err)
+		http.Error(w, "Erreur serveur", http.StatusInternalServerError)
+	}
+}
